2fdc03b36731ffdc449131ab4db53dea1b119baa
TAHK6e3TOPMO3OB <kirill_stefanenkov@rambler.ru>
2016-11-04 23:05:49 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -6,3 +6,10 @@ SCENARIO("ctor1", "[ctor1]"){
   shared_ptr<int> sp;
   REQUIRE(!sp.get());
 }
+
+SCENARIO("ctor2", "[ctor2]"){
+  shared_ptr<int> sp(new int(7));
+  REQUIRE(*sp==7);
+  REQUIRE(sp.countref()==1);
+}
+
